3a3ad7fc8aa908606d7e06e434da7b88d0dcb053
worldwarmworm <st.valeriq@gmail.com>
2023-05-19 15:10:54 +0700

    set Routes and Middlewares

---
+++ app/Http/Controllers/User/PostController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+
+class PostController extends Controller
+{
+	public function index(): string
+	{
+		return 'Страница списка постов';
+	}
+
+	public function create(): string
+	{
+		return 'Страница создания поста';
+	}
+
+	public function store(): string
+	{
+		return 'Запрос сохранения поста';
+	}
+
+	public function show($id): string
+	{
+		return "Страница просмотра поста $id";
+	}
+
+	public function edit($id): string
+	{
+		return "Страница изменения поста $id";
+	}
+
+	public function update($id): string
+	{
+		return "Запрос изменения поста $id";
+	}
+
+	public function delete($id): string
+	{
+		return "Запрос удаления поста $id";
+	}
+
+	public function like($id): string
+	{
+		return "Лайк + 1 для поста $id";
+	}
+}
---
+++ routes/main.php
@@ -0,0 +1,17 @@
+<?php
+;
+use App\Http\Controllers\BlogController;
+use App\Http\Controllers\Posts\CommentController;
+use Illuminate\Support\Facades\Route;
+
+Route::view('/', 'welcome');
+
+Route::get('blog', [BlogController::class, 'index'])->name('blog');
+Route::get('blog/{post}', [BlogController::class, 'show'])->name('blog.show');
+Route::post('blog/{post}/like', [BlogController::class, 'like'])->name('blog.show.like');
+
+Route::resource('posts/{post}/comments', CommentController::class)->only(['index', 'show']);
+
+
+
+//Route::fallback(static fn() => 'Error 404');
