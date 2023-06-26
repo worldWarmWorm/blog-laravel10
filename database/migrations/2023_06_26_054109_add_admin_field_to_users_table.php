4dce55ef8995835aec91393dfe47c0296ac1d6f7
worldwarmworm <st.valeriq@gmail.com>
2023-06-26 23:03:01 +0700

    WIP 28.06.23 22:58

---
+++ database/migrations/2023_06_26_054109_add_admin_field_to_users_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+	public function up(): void
+	{
+		Schema::table(
+			'users',
+			static function (Blueprint $table) {
+				$table->boolean('admin')->default(false)->after('active');
+			}
+		);
+	}
+
+	public function down(): void
+	{
+		Schema::table(
+			'users',
+			static function (Blueprint $table) {
+				$table->dropColumn('admin');
+			}
+		);
+	}
+};
